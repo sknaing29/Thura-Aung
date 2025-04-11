7e63aa6ee7326d263da1eb2fc2a894fce9ed14ba
sknaing29 <164123908+sknaing29@users.noreply.github.com>
2025-04-11 14:09:11 +0630

    Add files via upload

---
+++ 3333.js
@@ -0,0 +1,7 @@
+// Ensure calculateAll function is loaded from 2222.js before this runs
+
+// Run the calculation once on page load
+calculateAll();
+
+// Re-run the calculation every second to update the time
+setInterval(calculateAll, 1000);
